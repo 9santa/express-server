e818be9d558624fd5b49d0bc4bd8f8d692ed2f55
9santa <wotegorb@mail.ru>
2025-02-02 11:41:23 +0300

    add nodemon

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express"
 const app = express()
-const port = 3000
+const port = 2555
 
 app.listen(port, () => {
 	console.log("The server is listening at port " + port + ".")
 })
 
 app.get("/", (req, res) => {
-	res.send("Hello World!")
+	res.send("Hello Express!")
 })
